6b314e6d4f059bf522c6709b38fd8e0b7e5f7d42
Brandon Van Dyck <bvandyck@shyftanalytics.com>
2018-12-19 12:03:03 -0500

    Remove console error

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import BookGrid from './BookGrid';
 import Shelf from './Shelf';
 
 class Bookshelf extends Component {
