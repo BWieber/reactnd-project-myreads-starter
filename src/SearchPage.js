72281c25952c78be4b69539aefa716ebbde1b7a8
Brandon Van Dyck <bvandyck@shyftanalytics.com>
2019-02-03 16:43:19 -0500

    feat: only set books if query matches and return a No Books Found message

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -4,79 +4,79 @@ import React, { Component } from 'react';
 import Book from './Book';
 
 class SearchPage extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        query: '',
-        books: []
-      }
-
-      this.updateQuery = this.updateQuery.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      books: []
     }
 
-    updateQuery = (query) => {
-      const isSpaceOrDelete = (query.split('').pop() === ' ') || query.trim() === this.state.query.trim();
+    this.updateQuery = this.updateQuery.bind(this);
+  }
 
-      this.setState({ query: query })
-      if (isSpaceOrDelete) {
-        return;
-      }
+  updateQuery = (query) => {
+    const isSpaceOrDelete = (query.split('').pop() === ' ') || query.trim() === this.state.query.trim();
 
-      if(query === '') {
-        this.setState({ books: [] })
-      } else {
-        BooksAPI.search(query).then((books) => {
-          if(query === this.state.query) {
-            this.setState({ books })
-          }
-        })
-      }
+    this.setState({ query: query })
+    if (isSpaceOrDelete) {
+      return;
     }
 
-    clearQuery = () => {
-      this.setState({ query: '' })
+    if (query === '') {
+      this.setState({ books: [] })
+    } else {
+      BooksAPI.search(query).then((books) => {
+        if (query === this.state.query) {
+          this.setState({ books })
+        }
+      })
     }
+  }
 
-    render() {
-        const { query, books } = this.state;
-        const { shelfBooks, updateBookLocation } = this.props;
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
 
-        return (
-            <div className="search-books">
-            <div className="search-books-bar">
-              <Link
-                to="/"
-              > <button className="close-search">Close</button> </Link>
+  render() {
+    const { query, books } = this.state;
+    const { shelfBooks, updateBookLocation } = this.props;
 
-              
-              <div className="search-books-input-wrapper">
-                <input 
-                  type="text" 
-                  placeholder="Search by title or author"
-                  value={query}
-                  onChange={(event) => this.updateQuery(event.target.value)}
-                />
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link
+            to="/"
+          > <button className="close-search">Close</button> </Link>
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid">
-              {books.length > 0
-              ? books.map((book) => 
+
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {books.length > 0
+              ? books.map((book) =>
                 <li key={book.id}>
-                  <Book 
+                  <Book
                     updateBookLocation={updateBookLocation}
                     book={book}
                     shelfBooks={shelfBooks}
                   />
-                 </li>
-                )
+                </li>
+              )
               : <div> No Books Found </div>}
-              </ol>
-            </div>
-          </div>
-        )
-    }
+          </ol>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default SearchPage
